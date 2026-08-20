4a2f086c4d5b17deb6b2e60e917a09a3d69eb65c
livialossavaro <livialossavaro7@gmail.com>
2026-08-20 08:59:59 -0300

    0.1.1 - error fix

--- java.js
+++ java.js
@@ -4,8 +4,9 @@ document.getElementById("Botao").onclick = () => {
     contador ++;
     let toast = document.getElementById("toast");
     if (!toast) {
-        toast = document.getElementById("toast");
+        toast = document.createElement("div");
         toast.id = "toast";
+        toast.className = "toast";
         document.body.appendChild(toast);
     }
     toast.textContent = `Voce clicou ${contador}`;
